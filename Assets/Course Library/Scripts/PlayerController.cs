cb79dd6884e54a1c4031d4f678132a55ebc5d58a
Riley Ruiz <56165453+xLuna844x@users.noreply.github.com>
2019-11-27 08:15:27 -0800

    Objects will be destroyed

--- Assets/Course Library/Scripts/PlayerController.cs
+++ Assets/Course Library/Scripts/PlayerController.cs
@@ -6,7 +6,8 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
-    public float xRange = 10;
+    public float xRange = 10; 
+    public GameObject projectilePrefab;
     
     // Start is called before the first frame update
     void Start()
@@ -25,6 +26,11 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }    
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+          //Launch a projectile from the player
+          Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }   
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
